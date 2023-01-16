337c979566d5f18c84be38b9f87669862c92723f
vtb4055920 <vtb4055920@region.vtb.ru>
2023-01-16 22:30:37 +0300

    добавлены тесты

--- src/test/java/ru/practicum/shareit/request/RequestRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/request/RequestRepositoryTest.java
@@ -17,7 +17,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 @SpringBootTest
 //@Transactional
 class RequestRepositoryTest {
