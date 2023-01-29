3083554d6509c7aad9160f22f95d850b0d98f9c4
vtb4055920 <vtb4055920@region.vtb.ru>
2023-01-29 18:22:23 +0300

    fix compose

--- server/src/test/java/ru/practicum/shareit/request/RequestRepositoryTest.java
+++ server/src/test/java/ru/practicum/shareit/request/RequestRepositoryTest.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.request;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Propagation;
